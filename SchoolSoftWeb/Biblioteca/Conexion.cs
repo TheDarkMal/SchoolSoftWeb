c40d91675b0f3cf113432abb601004fb5eef543e
Mauricio Vergara <mauricio301051020@gmail.com>
2019-09-02 12:02:18 -0500

    conexion a base de datos mediante una biblioteca

---
+++ SchoolSoftWeb/Biblioteca/Conexion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Biblioteca
+{
+    class Conexion
+    {
+        public MySqlConnection getconexion() {
+            try {
+
+                string cadena = "datasource=localhost;port=3306;username=root;database=prueba1;";
+                MySqlConnection con = new MySqlConnection(cadena);
+                con.Open();
+                return con;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ SchoolSoftWeb/Biblioteca/Operaciones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace Biblioteca
+{
+    class Operaciones
+    {
+
+        public bool insert(string nombre,string apellidos , int celular)
+        {
+            Conexion cn = new Conexion();
+            try
+            {
+                string mysql = "insert into ejemplo.nombres (nombre, documento, tipodocumento) values (?nombre, ?documento, ?tipodocumento);";
+                MySqlCommand cmd = new MySqlCommand(mysql,cn.getconexion());
+                int n = cmd.ExecuteNonQuery();
+                return n > 0;
+            }
+            catch (Exception) {
+
+                return false;
+            }
+        }
+    }
+}
